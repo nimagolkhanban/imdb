436e6dcd08c5fdc669059c8c2867ade5050306e4
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-04-10 16:00:36 +0330

    login test

--- user_app/tests.py
+++ user_app/tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class RegisterTestCase(APITestCase):
@@ -17,3 +17,17 @@ class RegisterTestCase(APITestCase):
         
         response = self.client.post(reverse("register"), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+
+class LoginLogoutTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username="example", password="newpasswprd@123")
+    
+    def test_login(self):
+        data = {
+            "username": "example",
+            "password": "newpasswprd@123"
+        }
+        response = self.client.post(reverse("login"), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
